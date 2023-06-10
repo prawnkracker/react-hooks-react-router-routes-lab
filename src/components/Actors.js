7b57c3072569fc2a4aea57d7de56d6e4bbaff45b
Dennis Demir <dennisjdemir@gmail.com>
2023-06-10 14:14:46 -0400

    complete lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => {
+        return <div key={actor.name}> 
+        {actor.name} {actor.movies.map((movie, index) => <ul key={index}><li>{movie}</li></ul>)} 
+        </div>
+      })}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,16 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return <div key={movie.title}> 
+        {movie.title} {movie.time} {movie.genres.map((genre, index) => <ul key={index}><li >{genre}</li></ul>)} 
+        </div>
+      })}
+    </div>
+  );
 }
 
 export default Movies;
